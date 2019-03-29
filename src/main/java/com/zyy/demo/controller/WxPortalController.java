a45c6e119eb094c053be29093ecc0ea48a41a557
zhaiyongyi <zhaiyongyi@henhaoji.com>
2019-03-29 11:31:38 +0800

    config

--- src/main/java/com/zyy/demo/controller/WxPortalController.java
+++ src/main/java/com/zyy/demo/controller/WxPortalController.java
@@ -1,4 +1,9 @@
 package com.zyy.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wx/portal/{appid}")
 public class WxPortalController {
 }
