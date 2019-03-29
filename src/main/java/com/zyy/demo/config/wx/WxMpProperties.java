c370a4c7b9fc305cefb506a3a6040d8791ca45d1
zhaiyongyi <zhaiyongyi@henhaoji.com>
2019-03-29 19:45:17 +0800

    处理@ConfigurationProperties异常问题

--- src/main/java/com/zyy/demo/config/wx/WxMpProperties.java
+++ src/main/java/com/zyy/demo/config/wx/WxMpProperties.java
@@ -2,10 +2,12 @@ package com.zyy.demo.config.wx;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Data
+@Component
 @ConfigurationProperties(prefix = "wx.mp")
 public class WxMpProperties {
 
