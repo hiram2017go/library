cfd825ca1bf45fb45e8f006f22223e1f2a672ec1
zhaiyongyi <zhaiyongyi@henhaoji.com>
2019-04-17 11:27:10 +0800

    修改bug

--- src/main/java/com/zyy/demo/controller/UserController.java
+++ src/main/java/com/zyy/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.zyy.demo.dao.UserDao;
 import com.zyy.demo.entity.User;
 import com.zyy.demo.service.UserService;
 import com.zyy.demo.util.ImageUtils;
+import com.zyy.demo.util.RedisUtil;
 import com.zyy.demo.util.wx.CheckUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -23,6 +24,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisUtil redisUtil;
 
     @RequestMapping(value="/get",method={RequestMethod.POST,RequestMethod.GET})
     @GetMapping(produces = "text/plain;charset=utf-8")
@@ -49,5 +53,18 @@ public class UserController {
         ImageUtils.pressText("D:\\12345.jpg", "阿拉卡夏之眼", "宋体", Font.BOLD|Font.ITALIC, 20, Color.red, 300, 500, 0.6f);
 
         return "添加已完成";
+    }
+
+    @RequestMapping(value = "/message", method = {RequestMethod.GET, RequestMethod.POST})
+    @GetMapping(produces = "text/plain;charset=utd-8")
+    public String broadcastMessage() throws InterruptedException {
+        redisUtil.set("accessToken", "abcdef");
+
+        for(int i = 0; i < 80; i++){
+            String result = redisUtil.get("accessToken", String.class);
+            System.out.println("result = "+ result + i);
+            Thread.sleep(1000);
+        }
+        return "ok";
     }
 }
